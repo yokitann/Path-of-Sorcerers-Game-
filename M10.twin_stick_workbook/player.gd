7b152138388074f444bd01894a739fd78b611ce7
yokitann <yotan3@s.sfusd.edu>
2025-01-31 13:49:37 -0800

    L10 pt 2

--- M10.twin_stick_workbook/player.gd
+++ M10.twin_stick_workbook/player.gd
@@ -3,6 +3,7 @@ class_name Player extends CharacterBody2D
 @export var speed := 460.0
 @export var steering_factor := 10.0
 @export var max_health := 100
+@onready var _HealthBar: ProgressBar = %HealthBar
 var health := max_health: set = setter_health
 
 func _physics_process(delta: float) -> void:
@@ -11,6 +12,10 @@ func _physics_process(delta: float) -> void:
 	var steering := desired_velocity - velocity
 	velocity += steering * steering_factor * delta
 	move_and_slide()
+
+func _ready() -> void: 
+	_HealthBar.max_value = max_health
+	_HealthBar.value = health 
 
 func setter_health(new_health: int) -> void: 
 	var previous_health := health
