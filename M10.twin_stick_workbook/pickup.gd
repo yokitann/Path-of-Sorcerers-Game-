2e07e9cfbaed85f4c04c258890b799cf991d8b66
yokitann <yotan3@s.sfusd.edu>
2025-02-03 15:47:13 -0800

    L11 (fixing)

--- M10.twin_stick_workbook/pickup.gd
+++ M10.twin_stick_workbook/pickup.gd
@@ -4,12 +4,18 @@ extends Area2D
 @export var item: Item = null: set = set_item
 @onready var _gem: Sprite2D = %Gem
 @onready var _pickup_sound: AudioStreamPlayer2D = %PickupSound
+@onready var _animation: AnimationPlayer = %AnimationPlayer
 
 func _ready() -> void:
 	set_item(item)
+	_animation.play("idle")
 	body_entered.connect(func (body: Node2D) -> void:
 		if body is Player:
 			item.use(body)
+			_pickup_sound.play()
+			_animation._animation.stop().connect(func (_animation_name: String) -> void:
+				queue_free()
+			)
 		)
 
 func set_item(value: Item) -> void:
